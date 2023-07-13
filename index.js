01ac639d359b6f181ac12dbf3560a5b3f14ea835
Ale Candia <alejandrogcandia@gmail.com>
2023-07-13 12:08:25 -0300

    se configura sequelize en src/app.js se inicia base de datos

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const server = require('./src/app.js')
+const {conn} = require('./src/db.js')
 require('dotenv').config();
 const {PORT} = process.env;
-server.listen(PORT,()=>{
-    console.log('Servidor Listo en puerto : '+PORT)
+
+//config init
+conn.sync().then(()=>{
+
+    server.listen(PORT,()=>{
+        console.log('Servidor Listo en puerto : '+PORT)
+    });
+
 });
+
