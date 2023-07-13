fb35928d7d64810abde48cb71dc9557d6fdf1011
Ale Candia <alejandrogcandia@gmail.com>
2023-07-13 15:36:02 -0300

    creando ruta /formulario

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 //improtaciones
 const {Router} = require('express');
 const handler_home = require('./handlers/handler_home.js')
+const handle_set_form = require('./handlers/handle_set_form.js')
 // instancias
 const router =  Router();
 //config
 
 router.get('/',handler_home);
+router.set('/formulario',handle_set_form);
 
 //export
 module.exports=router;
