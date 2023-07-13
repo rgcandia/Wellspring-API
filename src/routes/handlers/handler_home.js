067dd78dab488242c16b0d727f446d6ac8151076
Ale Candia <alejandrogcandia@gmail.com>
2023-07-13 14:51:44 -0300

    prueba2

--- src/routes/handlers/handler_home.js
+++ src/routes/handlers/handler_home.js
@@ -1,8 +1,5 @@
 require('dotenv').config();
-
-const {POSTGRES_DATABASE,POSTGRES_PASSWORD,POSTGRES_HOST,POSTGRES_USER} = process.env;
-
 module.exports = (req,res)=>{
 
-    res.status(200).send(POSTGRES_DATABASE+''+POSTGRES_PASSWORD+''+POSTGRES_HOST+''+POSTGRES_USER)
+    res.status(200).send('Servidor corriendo correctamente')
 }
